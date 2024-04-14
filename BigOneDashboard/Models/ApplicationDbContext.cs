4ccaf1c591def3bd02507f88f5e34514df7c799e
sherg <shergertons@gmail.com>
2024-04-14 16:20:05 -0400

    WIP working web application, going to connect bot to DB

---
+++ BigOneDashboard/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using System.Collections.Generic;
+
+//public class ApplicationDbContext : DbContext
+//{
+//    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+//    {
+//    }
+
+//    public DbSet<YourModel> YourModels { get; set; }
+//}
