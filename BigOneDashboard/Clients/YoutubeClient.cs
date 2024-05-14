fe8aea4ef0d20058a84a0d9f7d9f194c1be28ecb
Shaun Hergerton <shergertons@gmail.com>
2024-05-13 23:53:41 -0400

    Fixed build error

--- BigOneDashboard/Clients/YoutubeClient.cs
+++ BigOneDashboard/Clients/YoutubeClient.cs
@@ -5,7 +5,6 @@ namespace BigOneDashboard.Clients
     public interface IYoutubeClient
     {
         string GetVideoIdFromUrl(string url);
-        Task<string> GetEmbedFromUrl(string url);
     }
     public class YoutubeClient(
         HttpClient httpClient) : IYoutubeClient
@@ -15,11 +14,6 @@ namespace BigOneDashboard.Clients
             var uri = new Uri(url);
             var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
             return query["v"];
-        }
-
-        public async Task<string> GetEmbedFromUrl(string url)
-        {
-
         }
     }
 }
