53bc59a99441acafce25f08c2974735b8641df87
Shaun Hergerton <SHergerton@townfair.com>
2024-05-20 09:17:47 -0400

    Created partials for list items

---
+++ BigOneDashboard/Views/Shared/SoundPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigOneDashboard.Views.Shared
+{
+    public class SoundPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BigOneDashboard/Views/Shared/_HistoryItemPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigOneDashboard.Views.Shared
+{
+    public class _HistoryItemPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
